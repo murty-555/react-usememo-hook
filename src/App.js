a4fc4c6e4d6f9bc6eb52b4160d266c6c604a88fb
murty-555 <krlvmurthy96@gmail.com>
2024-03-11 11:27:44 +0530

    used useMemo which stop unnecessary re-renders

--- src/App.js
+++ src/App.js
@@ -9,13 +9,13 @@ function App() {
     return counter * counter
   }
 
-  // const memoizedSquareValue = useMemo(counterSqr, [counter])
+  const memoizedSquareValue = useMemo(counterSqr, [counter])
 
   return (
     <div className="App">
       <h3>Counter: {counter}</h3>
       <button onClick={() => setCounter(counter + 1)}>Increment</button>
-      <h3>Counter Square: {counterSqr()}</h3>
+      <h3>Counter Square: {memoizedSquareValue}</h3>
       <br/>
       <h3>Counter 2: {counter2}</h3>
       <button onClick={() => setCounter2(counter2 - 1)}>Decrement</button>
